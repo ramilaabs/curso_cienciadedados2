c95bdf162f22bb9abe5390c45753d72de619ef76
Ramila <sousaramila7@gmail.com>
2026-04-27 14:53:04 -0300

    atualização do script

--- Scripts/trabalho_cienciadados2.R
+++ Scripts/trabalho_cienciadados2.R
@@ -9,16 +9,13 @@ library(viridis)
 library(ggpubr)
 
 # Carregar dados ---------------------------------------------------------
-load('Dados/indicadores_vulclim.Rdata')
+load('Dados/dados_indicadores.Rdata')
 
 # Visualização Avançada 1 ----------------------------------------------------
 ### Fazer um gráfico de densidade para cada UF, separando por região para visualizar 
 #a distribuição do indicador de vulnerabilidade climática relacionada à estiagem hídrica (IND_VULC_ESTRE_HIDR) por região (REGIAO).
-<<<<<<< HEAD
+
 ggplot(dados_completos, aes (x = IND_VULC_ESTRE_HIDR, y = UF, fill = REGIAO)) +
-=======
-g1 <- ggplot(dados_completos, aes (x = IND_VULC_ESTRE_HIDR, y = UF, fill = REGIAO)) +
->>>>>>> 895ea4723d876c81ffe9f2cc28566e3bb96246be
   geom_density_ridges(alpha = 0.7) +
   facet_wrap(REGIAO ~ ., scales = "free_y", space = "free_y") +
   labs(x = "Indicador de Vulnerabilidade Climática",
@@ -30,19 +27,12 @@ g1 <- ggplot(dados_completos, aes (x = IND_VULC_ESTRE_HIDR, y = UF, fill = REGIA
     legend.position = "none",                               
     axis.text.y = element_text(size = 10)
   )
-<<<<<<< HEAD
-=======
-g1  
->>>>>>> 895ea4723d876c81ffe9f2cc28566e3bb96246be
 
 
 # Visualização Avançada 2 -------------------------------------------------
 # Criando o gráfico avançado com correlação estatística automática
-<<<<<<< HEAD
+
 ggscatter(dados_completos, x = "IND_VULC_ESTRE_HIDR", y = "IND_RISCO_ARB",
-=======
-g2 <- ggscatter(dados_completos, x = "IND_VULC_ESTRE_HIDR", y = "IND_RISCO_ARB",
->>>>>>> 895ea4723d876c81ffe9f2cc28566e3bb96246be
           color = "REGIAO", palette = "Dark2",
           shape = "REGIAO",
           size = 1.5, alpha = 0.4,
@@ -64,11 +54,7 @@ g2 <- ggscatter(dados_completos, x = "IND_VULC_ESTRE_HIDR", y = "IND_RISCO_ARB",
   facet_wrap(~REGIAO) +
   theme (legend.position = "none")
 
-<<<<<<< HEAD
-=======
-g2
 
->>>>>>> 895ea4723d876c81ffe9f2cc28566e3bb96246be
 # Visualizar num gráfico interativo ---------------------------------------
 ### Criar um mapa interativo usando a biblioteca leaflet para visualizar 
 #a distribuição do indicador de vulnerabilidade climática relacionada à estiagem hídrica (IND_VULC_ESTRE_HIDR) por município.
@@ -77,8 +63,9 @@ g2
 municipios <- read_municipality(code_muni = "all", year = 2020)
 municipios$code_muni <- as.character(municipios$code_muni)
 
-saveRDS(municipios, "Dados/municipios.rds")#salavr em RDS para evitar ler o shapefile toda vez no Rmarkdown
-municipios <- readRDS("Dados/municipios.rds")
+saveRDS(municipios, "Dados/dados_municipios.rds") #salavr em RDS para evitar ler o shapefile toda vez no Rmarkdown
+municipios <- readRDS("Dados/dados_municipios.rds")
+
 # Juntar os dados de vulnerabilidade climática com os dados geográficos
 dados_municipios <- municipios %>%
   left_join(dados_completos, by = c("code_muni" = "CD_MUN"))
@@ -92,11 +79,7 @@ pal_viridis <- colorNumeric(
   na.color = "transparent"
 )
 
-<<<<<<< HEAD
 leaflet(dados_municipios) %>%
-=======
-g3 <- leaflet(dados_municipios) %>%
->>>>>>> 895ea4723d876c81ffe9f2cc28566e3bb96246be
   addProviderTiles(providers$CartoDB.Positron) %>%
   addPolygons(fillColor = ~pal_viridis(IND_VULC_ESTRE_HIDR),
               fillOpacity = 0.85,
@@ -106,11 +89,7 @@ g3 <- leaflet(dados_municipios) %>%
               popup = ~paste("Município:", NM_MUNICIPIO, "<br>",
                              "Vulnerabilidade:", round(IND_VULC_ESTRE_HIDR, 3))) %>%
   addLegend(pal = pal_viridis, values = ~IND_VULC_ESTRE_HIDR, title = "Vulnerabilidade Climática <br> ao Estresse Hídrico", position = "bottomright", opacity = 0.8)
-<<<<<<< HEAD
 
-=======
-g3
->>>>>>> 895ea4723d876c81ffe9f2cc28566e3bb96246be
 
 
 
